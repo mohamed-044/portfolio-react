9826ce047ae243c388007c0a5bb96a1552495806
mohamed-044 <mohamed.boulatika04@gmail.com>
2024-10-16 23:33:14 +0200

    Updates

---
+++ precache-manifest.b2755c26ae6c6c3d601c88943f119a9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee0186ac1a394d085d9ba807fb93c71b",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "6b2c79631f512639a428",
+    "url": "/portfolio-react/static/css/2.576ce916.chunk.css"
+  },
+  {
+    "revision": "751a4ca2d4d9a47dec01",
+    "url": "/portfolio-react/static/css/main.093238dd.chunk.css"
+  },
+  {
+    "revision": "6b2c79631f512639a428",
+    "url": "/portfolio-react/static/js/2.95fa0f3a.chunk.js"
+  },
+  {
+    "revision": "751a4ca2d4d9a47dec01",
+    "url": "/portfolio-react/static/js/main.b795b2b5.chunk.js"
+  },
+  {
+    "revision": "11528ddda4154ce1e32b",
+    "url": "/portfolio-react/static/js/runtime~main.72b3206a.js"
+  },
+  {
+    "revision": "f844ce6bf3db227adbdf43c6789e1e7c",
+    "url": "/portfolio-react/static/media/photo1.f844ce6b.jpg"
+  }
+]);
